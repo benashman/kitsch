2c95a820164de2210d0d6aa96c3fd88c3f3a62f3
Ben Ashman <ben.ashman@me.com>
2016-01-31 19:20:51 -0800

    Install ForecastIO pod

---
+++ Pods/Target Support Files/Pods-Kitsch/Pods-Kitsch-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Kitsch : NSObject
+@end
+@implementation PodsDummy_Pods_Kitsch
+@end
---
+++ Pods/Target Support Files/Pods-Kitsch/Pods-Kitsch-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_KitschVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_KitschVersionString[];
+
